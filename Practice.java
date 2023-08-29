af68e42bd39870922898664538771f15c190d6be
Arhaan Girdhar <123371085+17arhaan@users.noreply.github.com>
2023-08-30 01:06:53 +0530

    Add files via upload

---
+++ Practice.java
@@ -0,0 +1,13 @@
+class Practice 
+{  
+ public static void main(String[]args)
+  {
+    char ch;  
+    ch = 'A';
+      for (int i = 0 ; i <= 5 ; i++)
+      {
+        ch++;
+        System.out.println(ch );
+      }
+  }
+}
